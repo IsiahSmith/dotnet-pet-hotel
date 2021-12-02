5d3a1863d0c23a4412d9c538221d7b58f6e1d495
Ronald van der Bergh <rhvdbergh@gmail.com>
2021-12-02 14:01:49 -0600

    adds post route for petowners

--- Controllers/PetOwnersController.cs
+++ Controllers/PetOwnersController.cs
@@ -7,20 +7,37 @@ using Microsoft.EntityFrameworkCore;
 
 namespace pet_hotel.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class PetOwnersController : ControllerBase
+  [ApiController]
+  [Route("api/[controller]")]
+  public class PetOwnersController : ControllerBase
+  {
+    private readonly ApplicationContext _context;
+    public PetOwnersController(ApplicationContext context)
     {
-        private readonly ApplicationContext _context;
-        public PetOwnersController(ApplicationContext context) {
-            _context = context;
-        }
-
-        // This is just a stub for GET / to prevent any weird frontend errors that 
-        // occur when the route is missing in this controller
-        [HttpGet]
-        public IEnumerable<PetOwner> GetPets() {
-            return new List<PetOwner>();
-        }
+      _context = context;
     }
+
+    // This is just a stub for GET / to prevent any weird frontend errors that 
+    // occur when the route is missing in this controller
+    [HttpGet]
+    public IEnumerable<PetOwner> GetPets()
+    {
+      return new List<PetOwner>();
+    }
+
+    // POST /api/petowners
+    // expects a PetOwner object
+    [HttpPost]
+    public IActionResult Post(PetOwner petOwner)
+    {
+      // use the _context to speak to the db
+      _context.PetOwners.Add(petOwner);
+      // remember to save the changes
+      _context.SaveChanges();
+
+      // return the object that was created with a URL
+      return CreatedAtAction(nameof(Post), new { id = petOwner.id }, petOwner);
+    }
+
+  }
 }
--- Models/PetOwner.cs
+++ Models/PetOwner.cs
@@ -1,22 +1,22 @@
-using System.Collections.Generic;
+eusing System.Collections.Generic;
 using System;
 using System.Text.Json.Serialization;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 namespace pet_hotel
 {
-    public class PetOwner 
-    {
-        public int id { get; set; }
+  public class PetOwner
+  {
+    public int id { get; set; }
 
-        [Required]
-        public string name { get; set; }
+    [Required]
+    public string name { get; set; }
 
-        [Required]
-        [EmailAddress]
-        public string emailAddress { get; set; }
+    [Required]
+    [EmailAddress]
+    public string emailAddress { get; set; }
 
-        [NotMapped]
-        public int petCount { get; set; }
-    }
+    [NotMapped]
+    public int petCount { get; set; }
+  }
 }
