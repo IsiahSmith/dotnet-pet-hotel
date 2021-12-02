ddff907e6556dca474c17a2734a90b1206ccab6e
Isiah Smith <isiahthomassmith@gmail.com>
2021-12-02 13:37:54 -0600

    set up PetOwner model

--- Models/PetOwner.cs
+++ Models/PetOwner.cs
@@ -5,5 +5,18 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 namespace pet_hotel
 {
-    public class PetOwner {}
+    public class PetOwner 
+    {
+        public int id { get; set; }
+
+        [Required]
+        public string name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string emailAddress { get; set; }
+
+        [NotMapped]
+        public int petCount { get; set; }
+    }
 }
