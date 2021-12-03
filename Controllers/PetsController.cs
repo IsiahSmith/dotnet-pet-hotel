8b62acd11ac92ab1dc2ae1114f27b4dcb871bbbc
Isiah Smith <isiahthomassmith@gmail.com>
2021-12-03 10:27:02 -0600

    CheckedIn PUT made

--- Controllers/PetsController.cs
+++ Controllers/PetsController.cs
@@ -10,92 +10,112 @@ using Microsoft.EntityFrameworkCore;
 
 namespace pet_hotel.Controllers
 {
-  [ApiController]
-  [Route("api/[controller]")]
-  public class PetsController : ControllerBase
-  {
-    private readonly ApplicationContext _context;
-    public PetsController(ApplicationContext context)
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PetsController : ControllerBase
     {
-      _context = context;
-    }
-
-    [HttpPost]
-    public IActionResult Create(Pet pet)
-    {
-      //pet object is required on the request body
-      //must be JSON
-      Console.WriteLine("This is the post" + pet);
-      _context.Add(pet);
-
-      //save changes
-      _context.SaveChanges();
-
-      //respond
-      return CreatedAtAction(nameof(Create), new { id = pet.id }, pet);
-
-    }
-
-    // PUT to update pet information
-    [HttpPut("{id}")]
-    public IActionResult Put(int id, Pet pet)
-    {
-      Console.WriteLine("In PUT");
-
-      if (id != pet.id)
-      {
-        return BadRequest(); // 400
-      }
-
-      // update in DB
-      _context.Update(pet);
-      _context.SaveChanges();
-
-      return NoContent();
-    }
-
-    // GET /api/pets
-    // returns all pets
-    [HttpGet]
-    public IEnumerable<Pet> GetPets()
-    {
-      return _context.Pets;
-    }
-
-    // GET /api/pets/:id
-    // will return the pet at that id
-    [HttpGet("{id}")]
-    public ActionResult<Pet> GetById(int id)
-    {
-      Console.WriteLine("GET /api/pets/:id");
-      Pet pet = _context.Pets.SingleOrDefault(pet => pet.id == id);
-
-      // check that this is actually a pet object that has been returned
-      if (pet == null)
-      {
-        return NotFound();
-      }
-
-      return pet;
-    }
-
-    [HttpDelete("{id}")]
-    public IActionResult Delete(int id)
-    {
-        Console.WriteLine("Deleting with the id" + id);
-        Pet pet = _context.Pets.SingleOrDefault(pet => pet.id == id);
-
-        if(pet is null){
-            //not found
-            return NotFound();
+        private readonly ApplicationContext _context;
+        public PetsController(ApplicationContext context)
+        {
+            _context = context;
         }
 
-        //delete the pet
-        _context.Pets.Remove(pet);
-        _context.SaveChanges();
+        [HttpPost]
+        public IActionResult Create(Pet pet)
+        {
+            //pet object is required on the request body
+            //must be JSON
+            Console.WriteLine("This is the post" + pet);
+            _context.Add(pet);
 
-        //respond
-        return NoContent(); //204
+            //save changes
+            _context.SaveChanges();
+
+            //respond
+            return CreatedAtAction(nameof(Create), new { id = pet.id }, pet);
+
+        }
+
+        // PUT to update pet information
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Pet pet)
+        {
+            Console.WriteLine("In PUT");
+
+            if (id != pet.id)
+            {
+                return BadRequest(); // 400
+            }
+
+            // update in DB
+            _context.Update(pet);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        // GET /api/pets
+        // returns all pets
+        [HttpGet]
+        public IEnumerable<Pet> GetPets()
+        {
+            return _context.Pets;
+        }
+
+        // GET /api/pets/:id
+        // will return the pet at that id
+        [HttpGet("{id}")]
+        public ActionResult<Pet> GetById(int id)
+        {
+            Console.WriteLine("GET /api/pets/:id");
+            Pet pet = _context.Pets.SingleOrDefault(pet => pet.id == id);
+
+            // check that this is actually a pet object that has been returned
+            if (pet == null)
+            {
+                return NotFound();
+            }
+
+            return pet;
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            Console.WriteLine("Deleting with the id" + id);
+            Pet pet = _context.Pets.SingleOrDefault(pet => pet.id == id);
+
+            if (pet is null)
+            {
+                //not found
+                return NotFound();
+            }
+
+            //delete the pet
+            _context.Pets.Remove(pet);
+            _context.SaveChanges();
+
+            //respond
+            return NoContent(); //204
+        }
+
+        // PUT to set CheckedIn time of pet
+        [HttpPut("{id}/checkin")]
+        public IActionResult CheckedIn(int id, Pet pet)
+        {
+            Console.WriteLine("In CheckedIn PUT");
+
+            if (id != pet.id)
+            {
+                return BadRequest();
+            }
+
+            pet.checkedInAt = DateTime.Now;
+
+            _context.Pets.Update(pet);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
     }
-  }
 }
